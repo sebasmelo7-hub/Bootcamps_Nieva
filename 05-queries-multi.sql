16f68d880c0a863cc6dbd4285501e5467cc175f0
sebasmelo7-hub <sebas.melo7@gmail.com>
2026-08-27 15:33:26 -0500

    Queries 7-9

--- 05-queries-multi.sql
+++ 05-queries-multi.sql
@@ -1,3 +1,11 @@
+-- ======================================
+-- ENTREGABLE SEMANA 4 
+-- FASE 4 -  ENCADENAR TABLAS
+-- UNIVERSIDAD TECHMASTER - TECHMASTER
+-- Nombre: [PAUL SEBASTIAN MELO]
+-- Fecha: [27-08-2026]
+-- =====================================
+
 /*** FASE 4 - ENCADENAR TABLAS ***/
 /*
 ┌──────────┐      ┌─────────────┐      ┌─────────┐      ┌─────────────┐

477ddced66955802e457744439128a5dfbfaf744
sebasmelo7-hub <sebas.melo7@gmail.com>
2026-08-27 14:45:15 -0500

    Update Entregable_Semana4_SQLNieva_PaulMelo.sql

--- Entregable_Semana4_SQLNieva_PaulMelo.sql
+++ Entregable_Semana4_SQLNieva_PaulMelo.sql
@@ -986,7 +986,7 @@ order by s.name, e.course_id;
 -- "cursos con más inscritos"
 -- "inscritos" de verdad → excluye los withdrawn
 
-/* Validamos la lista de profesores */
+/* Validamos la lista de cursos */
 select *
 from courses;
 
@@ -1035,3 +1035,134 @@ join students    s on e.student_id = s.id
 group by c.code
 order by num_students desc
 limit 3;
+
+/** QUERY 14 - ESTUDIANTES INCRITOS EN CURSOS DE 2 O MÁS DEPARTAMENTOS DISTINTOS **/
+-- "estudiantes inscritos". "2+ departamentos"
+
+/* Validamos la lista de cursos */
+select *
+from courses;
+
+/* Validamos la lista de profesores */
+select *
+from students;
+
+/* Validamos la lista de departamentos */
+select *
+from departments;
+
+-- No existe relacion directa entre estudiantes - cursos
+-- Usamos inscripciones para unir estudiantes y cursos
+/* Validamos la lista de inscripciones */
+select *
+from enrollments;
+
+/* Usamos JOIN para traer estudiantes inscritos en cursos */
+/* 1. Usamos tabla enrollments para traer los estudiantes con course_id */
+select s.name as student, e.course_id
+from students    s
+join enrollments e on e.student_id = s.id
+order by s.id;
+
+/* 2. Usamos students - enrollments para traer los cursos */
+select s.name as student, c.code, c.name as course
+from students    s 
+join enrollments e on e.student_id = s.id
+join courses     c on e.course_id  = c.id
+order by s.id, c.code;
+
+/* 3. Usamos students - enrollments - courses para traer los departamentos */
+select s.name as student, c.code, c.name as course, d.name as department
+from students    s 
+join enrollments e on e.student_id    = s.id
+join courses     c on e.course_id     = c.id
+join departments d on c.department_id = d.id
+order by s.id, c.code;
+
+/* 4. Contamos departamentos unicos de cursos inscritos por c/estudiante */
+-- Usamos COUNT(DISTINCT ...) para contar departamentos unicos
+-- Usamos HAVING para filtrar despues del GROUP BY
+select s.name as student, count(distinct c.department_id) as num_deparmets
+from students    s 
+join enrollments e on e.student_id    = s.id
+join courses     c on e.course_id     = c.id
+group by s.name
+having count(distinct c.department_id) >=2
+order by num_deparmets desc;
+
+/** QUERY 15 - REPORTE EJECUTIVO POR DEPARTAMENTO **/
+-- Para cada departamento: total de profesores, total de cursos, y 
+-- total de estudiantes únicos inscritos en sus cursos
+-- on p.department_id = d.id
+
+/* Validamos la lista de departamentos */
+select *
+from departments;
+
+/* Validamos la lista de profesores */
+select *
+from professors;
+
+/* Validamos la lista de cursos */
+select *
+from courses;
+
+/* Validamos la lista de profesores */
+select *
+from students;
+
+-- No existe relacion directa entre estudiantes - cursos
+-- Usamos inscripciones para unir estudiantes y cursos
+/* Validamos la lista de inscripciones */
+select *
+from enrollments;
+
+/** Total de profesores - cursos - estudiantes para c/departamento **/
+/* 1. Usamos JOIN para traer total de profesores por c/departamento */
+-- Usamos COUNT(DISTINCT ...) para contar
+select 
+	d.name as department, 
+    count(distinct p.name) as num_professors
+from departments d
+join professors  p on p.department_id = d.id
+group by d.name, p.department_id
+order by num_professors desc, d.name;
+
+/* 2. Usamos JOIN para traer total de cursos por c/departamento */
+-- Usamos COUNT(DISTINCT ...) para contar
+select 
+	d.name as department, 
+    count(distinct p.name) as num_professors,
+    count(distinct c.code) as num_courses
+from departments d
+join professors  p on p.department_id = d.id
+join courses     c on c.department_id = d.id
+group by d.name
+order by num_professors desc, department;
+
+/* 3. Usamos JOIN para traer total de estudiantes por c/departamento */
+-- Usamos COUNT(DISTINCT ...) para contar
+select 
+	d.name as department, 
+    count(distinct p.name) as num_professors,
+    count(distinct c.code) as num_courses,
+    count(distinct s.id)   as num_students
+from departments      d
+join professors       p on p.department_id = d.id
+join courses          c on c.department_id = d.id
+left join enrollments e on e.course_id     = c.id
+left join students    s on e.student_id    = s.id
+group by d.name
+order by num_professors desc, num_students desc;
+
+select 
+	d.name as department, 
+    count(distinct p.name)       as num_professors,
+    count(distinct c.code)       as num_courses,
+    count(distinct e.student_id) as num_students
+from departments      d
+join professors       p on p.department_id = d.id
+join courses          c on c.department_id = d.id
+left join enrollments e on e.course_id     = c.id
+group by d.name
+order by num_professors desc, num_students desc;
